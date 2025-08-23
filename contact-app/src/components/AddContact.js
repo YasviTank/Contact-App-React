e1b678df156a910bde0dbd0a9b235a7ebdc847f6
Yasvi Tank <yasvitank@gmail.com>
2025-08-24 01:19:13 +0300

    Added onChange for the Add button

--- contact-app/src/components/AddContact.js
+++ contact-app/src/components/AddContact.js
@@ -5,11 +5,22 @@ class AddContact extends React.Component {
         name:"",
         email:""
     };
+
+        add = (e) => {
+            //To prevent default auto refresh
+            e.preventDefault();
+
+            if(this.state.name === "" && this.state.email === "") {
+                alert("All the fields are mandatory!");
+                return;
+            }
+            console.log(this.state);
+        };  
     render () {
         return (
             <div className="ui main">
                 <h2>Add Contact</h2>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.add}>
                     <div className="field">
                         <label>Name</label>
                         <input 
@@ -17,7 +28,7 @@ class AddContact extends React.Component {
                         name="name" 
                         placeholder="Name" 
                         value = {this.state.name}
-                        onchange={(e) => this.setState({name: e.target.value})} />
+                        onChange={(e) => this.setState({name: e.target.value})} />
                     </div>
                     <div className="field">
                         <label>Email</label>
@@ -26,7 +37,7 @@ class AddContact extends React.Component {
                         name="email" 
                         placeholder="Email"
                         value = {this.state.email}
-                        onchange={(e) => this.setState({email: e.target.value})} />
+                        onChange={(e) => this.setState({email: e.target.value})} />
                     </div>
                     <button className="ui button blue">Add</button>
                 </form>
